feba29ecf3308d062bc0e6a6a560ca3ee1a84efd
KNales179 <lucystar179@gmail.com>
2025-09-30 14:43:18 +0800

    Update Chat.js

--- routes/Chat.js
+++ routes/Chat.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const ChatMessage = require("../models/ChatMessage");
-const Driver = require("../models/Driver");
+const Driver = require("../models/Drivers");
 const Passenger = require("../models/Passenger");
 
 
